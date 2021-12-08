9d6a4ffdb06eeda54b3548d63708b4061e517b70
Alexey Lavrenovich <lavrenovichAE@gmail.com>
2021-12-08 21:04:18 +0300

    Initial commit

---
+++ Course Work/adder.v
@@ -0,0 +1,85 @@
+// This module implements both addition and subtraction
+module simd_adder(
+  input wire[255:0] A,
+  input wire[255:0] B,
+  input wire[2:0] data_mode,
+  input wire sub_flag,
+  input wire imm_flag,
+  input wire[7:0] imm_reg,
+  
+  output wire[255:0] out
+);
+  parameter SIMD_WIDTH = 256;
+  
+  reg[255:0] res;
+  assign out = res;
+  
+  integer i;
+  
+  always @(*) begin
+    if (imm_flag) begin
+      // Immediate operation
+      case(data_mode)
+        0: begin // 8bit
+          for (i=0; i < SIMD_WIDTH/8; i=i+1) begin
+            res[(i+1)*8 -:8] <= (sub_flag) ? A[(i+1)*8 -:8] - imm_reg: A[(i+1)*8 -:8] + imm_reg;
+          end
+        end
+        1: begin // 16bit
+          for (i=0; i < SIMD_WIDTH/16; i=i+1) begin
+            res[(i+1)*16 -:16] <= (sub_flag) ? A[(i+1)*16 -:16] - { 8{imm_reg[7]}, imm_reg}: A[(i+1)*16 -:16] + { 8{imm_reg[7]}, imm_reg};
+          end
+        end
+        2: begin // 32bit
+          for (i=0; i < SIMD_WIDTH/32; i=i+1) begin
+            res[(i+1)*32 -:32] <= (sub_flag) ? A[(i+1)*32 -:32] - { 24{imm_reg[7]}, imm_reg}: A[(i+1)*32 -:32] + { 24{imm_reg[7]}, imm_reg};
+          end
+        end
+        3: begin // 64bit
+          for (i=0; i < SIMD_WIDTH/64; i=i+1) begin
+            res[(i+1)*64 -:64] <= (sub_flag) ? A[(i+1)*64 -:64] - { 56{imm_reg[7]}, imm_reg}: A[(i+1)*64 -:64] + { 56{imm_reg[7]}, imm_reg};
+          end
+        end
+        4: begin // 128bit
+          for (i=0; i < SIMD_WIDTH/128; i=i+1) begin
+            res[(i+1)*128 -:128] <= (sub_flag) ? A[(i+1)*128 -:128] - { 120{imm_reg[7]}, imm_reg}: A[(i+1)*128 -:128] + { 120{imm_reg[7]}, imm_reg};
+          end
+        end
+        5: begin // 256bit
+          res <= (sub_flag) ? A - { 248{imm_reg[7]}, imm_reg}: A + { 248{imm_reg[7]}, imm_reg};
+        end
+      endcase
+    end else begin
+      // Register operation
+      0: begin // 8bit
+          for (i=0; i < SIMD_WIDTH/8; i=i+1) begin
+            res[(i+1)*8 -:8] <= (sub_flag) ? A[(i+1)*8 -:8] - B[(i+1)*8 -:8]: A[(i+1)*8 -:8] + B[(i+1)*8 -:8];
+          end
+        end
+        1: begin // 16bit
+          for (i=0; i < SIMD_WIDTH/16; i=i+1) begin
+            res[(i+1)*16 -:16] <= (sub_flag) ? A[(i+1)*16 -:16] - B[(i+1)*16 -:16]: A[(i+1)*16 -:16] + B[(i+1)*16 -:16];
+          end
+        end
+        2: begin // 32bit
+          for (i=0; i < SIMD_WIDTH/32; i=i+1) begin
+            res[(i+1)*32 -:32] <= (sub_flag) ? A[(i+1)*32 -:32] - B[(i+1)*32 -:32]: A[(i+1)*32 -:32] + B[(i+1)*32 -:32];
+          end
+        end
+        3: begin // 64bit
+          for (i=0; i < SIMD_WIDTH/64; i=i+1) begin
+            res[(i+1)*64 -:64] <= (sub_flag) ? A[(i+1)*64 -:64] - B[(i+1)*64 -:64]: A[(i+1)*64 -:64] + B[(i+1)*64 -:64];
+          end
+        end
+        4: begin // 128bit
+          for (i=0; i < SIMD_WIDTH/128; i=i+1) begin
+            res[(i+1)*128 -:128] <= (sub_flag) ? A[(i+1)*128 -:128] - B[(i+1)*128 -:128]: A[(i+1)*128 -:128] + B[(i+1)*128 -:128];
+          end
+        end
+        5: begin // 256bit
+          res <= (sub_flag) ? A - B: A + B;
+        end
+    end
+  end
+  
+endmodule
---
+++ Course Work/decoder.v
@@ -0,0 +1,49 @@
+// This module implements instruction decoder logic
+module decoder(
+  input wire[15:0] inst,
+  input wire clk,
+  
+  output reg[3:0] opcode,
+  output reg[2:0] data_mode,
+  output reg imm_flag,
+  output reg[7:0] imm
+);
+  /*
+  parameter NOP  	= 4'b0000;
+  parameter PADD 	= 4'b0001;
+  parameter PSUB 	= 4'b0010;
+  parameter PSLL 	= 4'b0011;
+  parameter PSRL 	= 4'b0100;
+  parameter PSRA 	= 4'b0101;
+  parameter PCMPEQ 	= 4'b0110;
+  parameter PCMPGT 	= 4'b0111;
+  parameter PUNPKGLO= 4'b1000;
+  parameter PUNPKGHI= 4'b1001;
+  */
+  
+  // This block prevents all invalid
+  // operations and converts them into
+  // NOP if needed.
+  always @(posedge clk)
+    begin
+      // if incorrect data mode set
+      // or opcode is not yet defined
+      if (inst[15:12] > 4'b1001 || inst[11:9] > 3'b101)
+        begin
+          // convert to NOP
+          opcode <= 4'b0000;
+          data_mode <= 3'b000;
+          imm_flag <= 1'b1;
+          imm <= {8{1'b0}};
+        end
+      else
+        begin
+          // decode as normal instruction
+          opcode <= inst[15:12];
+          data_mode <= inst[11:9];
+          imm_flag <= inst[8];
+          imm <= inst[7:0];
+        end
+    end
+  
+endmodule
